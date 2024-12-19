61c432b18a2d8ac7823b974ebe1897e03311e284
Rudolf Bruder <rudolf.bruder@slovar.sk>
2024-12-19 14:51:08 +0100

    Update ResponseException.php

--- src/Exceptions/ResponseException.php
+++ src/Exceptions/ResponseException.php
@@ -4,8 +4,16 @@ namespace Ravols\EverifinPhp\Exceptions;
 
 class ResponseException extends \Exception
 {
-    public static const CODE_ERRORS_IN_RESPONSE = 500;
-    public static const CODE_DATA_NOT_PRESENT = 501;
-    public static const CODE_META_MISSING = 502;
-    public static const CODE_STATUS_NOT_SUCCESSFUL = 503;
+    public const CODE_ERRORS_IN_RESPONSE = 500;
+    public const CODE_DATA_NOT_PRESENT = 501;
+    public const CODE_META_MISSING = 502;
+    public const CODE_STATUS_NOT_SUCCESSFUL = 503;
+
+    public array $errors = [];
+
+    public function __construct($message, $code, $errors = [])
+    {
+        $this->errors = $errors;
+        parent::__construct($message, $code);
+    }
 }
